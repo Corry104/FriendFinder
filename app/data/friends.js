f844b1e392ba6b96c478ddfe9c8e9ce18421f820
Jack <Jack@Corrados-MacBook-Pro.local>
2019-01-09 10:02:45 -0800

    change

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends' data.
+// ===============================================================================
+
+
+var friendsData = [{
+    name : "Shirley Temple",
+    photo : "https://1.bp.blogspot.com/-IfWWnyxiTVw/WnVJa6sy20I/AAAAAAAAkfA/WTfJzBaHyn0EpZ0Bo_aISXGOGaBI8HccQCLcBGAs/s1600/Shirley-Temple-Photo%2B1.JPG",
+    scores : [1, 2, 2, 4, 5, 3, 1, 3, 5, 3]
+},
+{
+    name : "Old Fashioned",
+    photo : "https://cdn.liquor.com/wp-content/uploads/2017/08/08074649/6-Rules-for-Drinking-Bourbone-bourbon-old-fashioned-720x720-slideshow.jpg",
+    scores : [4, 3, 1, 1, 1, 2, 5, 4, 3, 5]
+},
+{
+    name : "Manhattan",
+    photo : "https://cdn.liquor.com/wp-content/uploads/2012/09/04121600/bourbon-manhattan.jpg",
+    scores : [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},
+{
+    name : "Margarita",
+    photo : "http://shake-that.com/wp-content/uploads/2015/07/Margarita-780x780.jpg",
+    scores : [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+},
+{
+    name : "French 75",
+    photo : "https://www.seriouseats.com/recipes/images/2015/04/20150323-cocktails-vicky-wasik-french75-1500x1125.jpg",
+    scores : [3, 2, 5, 1, 2, 3, 1, 1, 2, 2]
+}];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsData;
---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require("express");
+
+var app = express();
+
+var friends = require("./app/data/friends");
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+module.exports = function(app) {
+
+    app.get("api/friends", function(req, res){
+        res.josn(friends);
+    });
+
+    app.post("/api/friends", function(req, res){
+        let foundFriend;
+        let minDiff = 9999;
+        var userAnswer = req.body;
+
+        userAnswer.routeName = friends.name.replace(/\s+/g, "").toLowerCase();
+      
+        console.log(userAnswer);
+      
+        characters.push(userAnswer);
+      
+        res.json(userAnswer);
+
+        for (let i = 0; i < friends.length; i++) {
+            let totDiff = 0;
+            for (let j =0; j < 10; j++) {
+
+            }
+        }
+    });
+}
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+
+
